b6f19438aa4775d6bda9ab14eb267bd96a180a28
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-05-19 01:27:32 +0100

    Created React App and installed dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Waitlist from './components/Waitlist/Waitlist';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1>Hello World!</h1>
+      <Waitlist />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Waitlist/Waitlist.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Waitlist.css';
+
+const Waitlist = () => {
+  return (
+    <div>Waitlist</div>
+  )
+}
+
+export default Waitlist
