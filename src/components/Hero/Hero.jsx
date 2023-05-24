17945499fd692b35453b06d58367a402649e8aa5
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-05-24 08:23:52 +0100

    Waitlist section and some edits in other sections

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,13 +6,13 @@ import './Hero.css';
 
 const Hero = () => {
   return (
-    <>
+    <div className='hero'>
         <NavbarComp />
         <div className='hero-phrase'>
             <h1>Unlock Your Digital Team:<br/>Find, Collaborate, and Build the Future<br/>Together</h1>
         </div>
         <Waitlist />
-    </>
+    </div>
   )
 }
 
--- src/components/Hero/Waitlist/Waitlist.jsx
+++ src/components/Hero/Waitlist/Waitlist.jsx
@@ -3,7 +3,13 @@ import './Waitlist.css';
 
 const Waitlist = () => {
   return (
-    <div>Waitlist</div>
+    <div className='waitlist'>
+      <p>Join the waitlist</p>
+      <form action="">
+        <input type="text" placeholder='Enter your email address...'/>
+        <button>Join</button>
+      </form>
+    </div>
   )
 }
 
