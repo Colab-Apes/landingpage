c48728b793df10c4ae6b2bf8d2f7d8a70fe6e688
Emmanuel Akinbo <akinbo.emmanuel@lmu.edu.ng>
2023-06-02 02:17:10 +0100

    Socials

--- src/components/Hero/Socials/Socials.jsx
+++ src/components/Hero/Socials/Socials.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React from 'react';
+
+import youtube from '../../../assets/socials/youtube.svg';
+import facebook from '../../../assets/socials/facebook.svg';
+import instagram from '../../../assets/socials/instagram.svg';
+import twitter from '../../../assets/socials/twitter.svg';
+import linkedin from '../../../assets/socials/linkedin.svg';
+
 import './Socials.css';
 
 const Socials = () => {
   return (
     <div className='socials'>
-        <p>Socials</p>
+
+      <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+        <img src={linkedin} alt="linkedin" className="icon"/>
+      </a>
+
+      <a href="https://www.twitter.com/co_labapes" target="_blank" rel="noopener noreferrer">
+        <img src={twitter} alt="twitter" className="icon"/>
+      </a>
+
+      <a href="https://www.instagram.com/colabapes" target="_blank" rel="noopener noreferrer">
+        <img src={instagram} alt="instagram" className="icon"/>
+      </a>
+
+      <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+        <img src={facebook} alt="facebook" className="icon"/>
+      </a>
+
+      <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer">
+        <img src={youtube} alt="youtube" className="icon"/>
+      </a>
     </div>
   )
 }
