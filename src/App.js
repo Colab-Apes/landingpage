215b6ffd0f6e6d24d6b043683015bbc0d48a2433
himarnoel <olaimarnoel@gmail.com>
2023-06-14 15:59:04 +0100

    started with tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Hero from './components/Hero/Hero';
+import LandingPage from './Pages/LandingPage';
 
 function App() {
   return (
     <>
-      <Hero />
+      <LandingPage />
     </>
   );
 }
