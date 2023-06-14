b647ad8cefe4be20d38736c38c3c7270a9398f68
himarnoel <olaimarnoel@gmail.com>
2023-06-14 21:36:09 +0100

    working on join button

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ import heroimage from "../assets/waitlist.svg";
 const Hero = () => {
   return (
     <div className="grid md:grid-cols-2 px-20 mt-20 justify-items-center ">
-      <div className="flex flex-col justify-between">
+      <div className="flex flex-col justify-around">
         <div className="flex items-center gap-x-4 ">
           <p className="text-main text-2xl font-bold">Coming soon</p>
           <div className=" bg-[#3B8004] w-[20rem] h-[0.2rem]"></div>
@@ -13,12 +13,14 @@ const Hero = () => {
           Together
         </p>
         <div className="flex flex-col">
-          <p className=" text-main">Join the wait list</p>
-          <div className="relative">
+          <p className=" text-main font-lato font-medium">Join the wait list</p>
+          <div className="relative mt-2">
             <input
               type="text"
-              className="h-[3rem] w-[20rem] rounded-[20px] focus:outline-none border-[#3B8004] border-2 bg-transparent"
+              placeholder="enter your email address..."
+              className="h-[3.8rem] placeholder:text-[#999999] text-lg font-medium w-[24rem] px-[1.3rem] rounded-[16px] focus:outline-none border-[#3B8004] border-2 bg-transparent"
             />
+            <button className="join font-lato font-semibold h-[2rem] w-[8rem] text-white">Join</button>
           </div>
         </div>
       </div>
