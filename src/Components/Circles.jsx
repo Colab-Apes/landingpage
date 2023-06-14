d45e02e25a18bcd4780bd4591adf6af64a3d71be
himarnoel <olaimarnoel@gmail.com>
2023-06-14 20:34:37 +0100

    working on text

--- src/Components/Circles.jsx
+++ src/Components/Circles.jsx
@@ -4,7 +4,7 @@ const Circles = () => {
   return (
     <div>
       <div className="absolute top-[-2rem] left-[-5rem] bg-[#054E12] h-[41rem] w-[41rem] flex justify-center items-center  rounded-full blur-[500px] "></div>
-      <div className="absolute top-[13rem]   blur-[150px] left-[0px] bg-[#C26AAA] h-[18rem] w-[18rem] rounded-full "></div>
+      <div className="absolute top-[2rem]   blur-[200px] left-[0px] bg-[#C26AAA] h-[14rem] w-[14rem] rounded-full "></div>
       <div className="absolute h-[20rem] w-[20rem] bottom-0 right-0 bg-[#054E12] blur-[250px]"></div>
     </div>
   );
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -4,16 +4,16 @@ import Circles from "./../Components/Circles";
 
 const LandingPage = () => {
   return (
-    <div className="px-20 h-full w-full bg-[#F5FFF7] font-lato">
+    <div className="px-20 h-screen w-full bg-[#F5FFF7] font-lato">
       <NavBar />
       <Circles />
-      <div className="grid md:grid-cols-2 px-40 mt-32">
+      <div className="grid md:grid-cols-2 px-20 mt-32">
         <div className="flex flex-col">
-          <div className="flex items-center">
+          <div className="flex items-center gap-x-4 ">
             <p className="text-main text-xl">Coming soon</p>
             <div className=" bg-[#3B8004] w-[20rem] h-[0.2rem]"></div>
           </div>
-          <p className="text-2xl text-main mt-3">
+          <p className="text-4xl text-main mt-3 font-semibold leading-9">
             Unlock Your Digital Team: Find, Collaborate, and Build the Future
             Together
           </p>
