1ede4627925e7c63c6a4eb62e03cf50987b0e2c3
himarnoel <olaimarnoel@gmail.com>
2023-06-14 20:11:11 +0100

    added components

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
+import Circles from "./../Components/Circles";
 
 const LandingPage = () => {
   return (
     <div className="px-20 h-full w-full bg-[#F5FFF7]">
       <NavBar />
-      
+      <Circles />
     </div>
   );
 };
