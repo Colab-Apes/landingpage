1b729df9057c78982c7394cf4e95393eeec259fa
himarnoel <olaimarnoel@gmail.com>
2023-06-14 23:46:48 +0100

    working

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = () => {
         <button className=" text-main rounded-2xl  w-[8rem] h-[2rem] font-bold">
           Sign in
         </button>
-        <button className="grad rounded-2xl  w-[8rem] h-[2rem] text-white  font-bold">
+        <button className="grad rounded-2xl  w-[8rem] h-[3rem] text-white hover:text-main  font-bold">
           Sign up
         </button>
       </div>
