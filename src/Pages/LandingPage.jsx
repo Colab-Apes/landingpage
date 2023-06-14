609be6d200c204aa405e2bad689f85eefe9452ab
himarnoel <olaimarnoel@gmail.com>
2023-06-14 20:43:21 +0100

    added an image

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import NavBar from "../Components/NavBar";
 import Circles from "./../Components/Circles";
+import Hero from "./../Components/Hero";
 
 const LandingPage = () => {
   return (
     <div className="px-20 h-full w-full bg-[#F5FFF7] font-lato">
       <NavBar />
       <Circles />
-    
+      <Hero />
     </div>
   );
 };
