d7003d4044c477f9960a60c60cda88725a14861e
himarnoel <olaimarnoel@gmail.com>
2023-06-15 00:28:25 +0100

    removed bug

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -24,7 +24,7 @@ const Hero = () => {
               placeholder="enter your email address..."
               className="h-[4rem]  placeholder:text-[#999999] text-lg font-medium w-[24rem] xl:w-[26rem] px-[1.3rem] rounded-[16px] focus:outline-none border-[#3B8004] border-2 bg-transparent"
             />
-            <button className="join absolute right-2 inset-y-1  font-lato font-semibold h-[3.5rem] w-[6rem] rounded-[16px] text-white">
+            <button className="join absolute right-2 inset-y-2  font-lato font-semibold h-[3rem] w-[6rem] rounded-[16px] text-white">
               Join
             </button>
           </div>
