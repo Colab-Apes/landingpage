ea5fcf5c9e36aa7f61ca50d17dd6004e13a65221
himarnoel <olaimarnoel@gmail.com>
2023-06-14 16:17:02 +0100

    added colors

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -4,6 +4,9 @@ const NavBar = () => {
   return (
     <nav className="flex justify-between">
       <img src={logo} alt="" />
+      <div className="na">
+        <button className="text">Sign in</button>
+      </div>
     </nav>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#054E12",
+        second: "#47019d",
+        three: "#e00256",
+        black: "#212121",
+        white: "#ffffff",
+        gray: "#808080e2",
+      },
+    },
   },
   plugins: [],
-}
+};
