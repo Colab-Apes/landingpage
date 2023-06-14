675b7409c930ea93e77ee7f18bf3f603fd90430c
himarnoel <olaimarnoel@gmail.com>
2023-06-14 16:28:47 +0100

    made changes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import logo from "../assets/logo.svg";
+import "./App.css";
 const NavBar = () => {
   return (
     <nav className="flex justify-between">
       <img src={logo} alt="" />
       <div className="na">
-        <button className="text text-main ">Sign in</button>
+        <button className=" text-main ">Sign in</button>
+        <button className="grad  text-main ">Sign up</button>
       </div>
     </nav>
   );
