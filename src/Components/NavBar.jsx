94b490a6903e13002b52eb69afb6932f1b18ddfd
himarnoel <olaimarnoel@gmail.com>
2023-06-14 19:41:12 +0100

    working on nav

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -5,11 +5,11 @@ const NavBar = () => {
   return (
     <nav className="flex justify-between font-lato">
       <img src={logo} alt="" />
-      <div className="na">
-        <button className=" text-main rounded-2xl  w-[10rem] h-[2rem]">
+      <div className="text-xl flex gap-x-3 items-center">
+        <button className=" text-main rounded-2xl  w-[8rem] h-[2rem] font-bold">
           Sign in
         </button>
-        <button className="grad rounded-2xl  w-[10rem] h-[2rem] text-white font-">
+        <button className="grad rounded-2xl  w-[8rem] h-[2rem] text-white  font-bold">
           Sign up
         </button>
       </div>
