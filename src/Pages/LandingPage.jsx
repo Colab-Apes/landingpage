8279e0227c0e789d12b7ed4f8f03258634775cf4
himarnoel <olaimarnoel@gmail.com>
2023-06-14 20:26:22 +0100

    still working on the landing page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -7,10 +7,16 @@ const LandingPage = () => {
     <div className="px-20 h-full w-full bg-[#F5FFF7] font-lato">
       <NavBar />
       <Circles />
-      <div className="grid md:grid-cols-2 px-10">
-        <div className="flex">
-          <p>Coming soon</p>
-          <div className="flex bg-[#3B8004] px-2 py-2"></div>
+      <div className="grid md:grid-cols-2 px-40 mt-32">
+        <div className="flex flex-col">
+          <div className="flex items-center">
+            <p className="text-main text-xl">Coming soon</p>
+            <div className=" bg-[#3B8004] w-[20rem] h-[0.2rem]"></div>
+          </div>
+          <p className="text-2xl text-main mt-3">
+            Unlock Your Digital Team: Find, Collaborate, and Build the Future
+            Together
+          </p>
         </div>
       </div>
     </div>
