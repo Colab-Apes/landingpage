c9ae43586705d5901a5ed204a04ef300d6fc0dff
himarnoel <olaimarnoel@gmail.com>
2023-06-14 22:52:41 +0100

    added  image

---
+++ src/Components/Socials.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Socials = () => {
+  return (
+    <div className="flex w-full bg-green-500 h-[2rem]  float-right">
+      sdfsdafsd
+    </div>
+  );
+};
+
+export default Socials;
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,12 +3,14 @@ import NavBar from "../Components/NavBar";
 import Circles from "./../Components/Circles";
 import Hero from "./../Components/Hero";
 import image from "../assets/hero.jpeg";
+import Socials from './../Components/Socials';
 
 const LandingPage = () => {
   return (
     <div className="px-20 h-screen w-full bg-[#F5FFF7] bg-no-repeat  font-lato bg-mobilehero md:bg-hero  bg-cover">
       <NavBar />
       <Hero />
+      <Socials />
     </div>
   );
 };
