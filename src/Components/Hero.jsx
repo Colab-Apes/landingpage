b10b33af8686cb01478e50b24f62da4b0dd1218e
himarnoel <olaimarnoel@gmail.com>
2023-06-14 20:37:06 +0100

    created a new components

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="grid md:grid-cols-2 px-20 mt-32">
+      <div className="flex flex-col">
+        <div className="flex items-center gap-x-4 ">
+          <p className="text-main text-2xl font-bold">Coming soon</p>
+          <div className=" bg-[#3B8004] w-[20rem] h-[0.2rem]"></div>
+        </div>
+        <p className="text-4xl text-main mt-3 font-bold leading-[1.5]">
+          Unlock Your Digital Team: Find, Collaborate, and Build the Future
+          Together
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -7,18 +7,7 @@ const LandingPage = () => {
     <div className="px-20 h-screen w-full bg-[#F5FFF7] font-lato">
       <NavBar />
       <Circles />
-      <div className="grid md:grid-cols-2 px-20 mt-32">
-        <div className="flex flex-col">
-          <div className="flex items-center gap-x-4 ">
-            <p className="text-main text-xl">Coming soon</p>
-            <div className=" bg-[#3B8004] w-[20rem] h-[0.2rem]"></div>
-          </div>
-          <p className="text-4xl text-main mt-3 font-semibold leading-9">
-            Unlock Your Digital Team: Find, Collaborate, and Build the Future
-            Together
-          </p>
-        </div>
-      </div>
+    
     </div>
   );
 };
