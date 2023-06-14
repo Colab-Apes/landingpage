99628790157302d80b600fda0c6ed529bed3b0e7
himarnoel <olaimarnoel@gmail.com>
2023-06-14 20:13:35 +0100

    made some corrections

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -4,9 +4,15 @@ import Circles from "./../Components/Circles";
 
 const LandingPage = () => {
   return (
-    <div className="px-20 h-full w-full bg-[#F5FFF7]">
+    <div className="px-20 h-full w-full bg-[#F5FFF7] font-lato">
       <NavBar />
       <Circles />
+      <div className="grid md:grid-cols-2 px-10">
+        <div className="flex">
+          <p>Coming soon</p>
+          <div className="flex bg-[#3B8004] px-2 py-2"></div>
+        </div>
+      </div>
     </div>
   );
 };
