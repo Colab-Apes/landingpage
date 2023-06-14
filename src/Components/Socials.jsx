ad4fb31f25d9955d2546fa53820419f66e44cc40
himarnoel <olaimarnoel@gmail.com>
2023-06-14 23:47:33 +0100

    socials

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -7,19 +7,34 @@ import linkedin from "../assets/socials/linkedin.svg";
 const Socials = () => {
   return (
     <div className="flex w-full  h-[10vh] items-center justify-end gap-x-3">
-      <a href="https://www.twitter.com/co_labapes" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a
+        href="https://www.youtube.com"
+        className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center"
+      >
         <img src={youtube} alt="" />
       </a>
-      <a href="https://www.twitter.com/co_labapes"  className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a
+        href="https://www.twitter.com/co_labapes"
+        className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center"
+      >
         <img src={facebook} alt="" />
       </a>
-      <a href="https://www.twitter.com/co_labapes" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a
+        href="https://www.instagram.com/co_labapes"
+        className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center"
+      >
         <img src={instagram} alt="" />
       </a>
-      <a href="https://www.twitter.com/co_labapes" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a
+        href="https://www.twitter.com/co_labapes"
+        className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center"
+      >
         <img src={twitter} alt="" />
       </a>
-      <a href="https://www.twitter.com/co_labapes" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a
+        href="https://www.twitter.com/co_labapes"
+        className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center"
+      >
         <img src={linkedin} alt="" />
       </a>
     </div>
