0a46c9a6cdc917f5e6bb9668972441dbf5c1f77c
himarnoel <olaimarnoel@gmail.com>
2023-06-14 22:12:16 +0100

    working on it

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import heroimage from "../assets/waitlist.svg";
 const Hero = () => {
   return (
-    <div className="grid md:grid-cols-2 px-20 mt-20 justify-items-center ">
-      <div className="flex flex-col justify-around">
+    <div className="grid md:grid-cols-2 px-20 h-[75vh] justify-center items-center justify-items-center ">
+      <div className="flex flex-col h-[30vh] justify-around">
         <div className="flex items-center gap-x-4 ">
           <p className="text-main text-2xl font-bold">Coming soon</p>
           <div className=" bg-[#3B8004] w-[20rem] h-[0.2rem]"></div>
         </div>
-        <p className="text-4xl text-main mt-3 font-bold leading-[1.5]">
+        <p className="text-4xl text-main mt-4 font-bold leading-[1.5]">
           Unlock Your Digital Team: Find, Collaborate, and Build the Future
           Together
         </p>
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-4 ">
           <p className=" text-[#054E12] text-xl font-lato font-medium">
             Join the wait list
           </p>
