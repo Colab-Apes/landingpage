d9d23e41d7edae57cf8a1b308f347805ccd752b3
himarnoel <olaimarnoel@gmail.com>
2023-06-14 23:27:36 +0100

    doing well

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,9 +1,27 @@
 import React from "react";
-
+import youtube from "../assets/socials/youtube.svg";
+import facebook from "../assets/socials/facebook.svg";
+import instagram from "../assets/socials/instagram.svg";
+import twitter from "../assets/socials/twitter.svg";
+import linkedin from "../assets/socials/linkedin.svg";
 const Socials = () => {
   return (
-    <div className="flex w-full bg-green-500 h-[2rem]  float-right">
-      sdfsdafsd
+    <div className="flex w-full  h-[2rem] items-center justify-end gap-x-3">
+      <a href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+        <img src={youtube} alt="" />
+      </a>
+      <a  href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+        <img src={facebook} alt="" />
+      </a>
+      <a  href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+        <img src={instagram} alt="" />
+      </a>
+      <a  href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+        <img src={twitter} alt="" />
+      </a>
+      <a  href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+        <img src={linkedin} alt="" />
+      </a>
     </div>
   );
 };
