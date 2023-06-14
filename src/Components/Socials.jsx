30b89cc1a15abb30dbcf116c8b203fc40e1d0df9
himarnoel <olaimarnoel@gmail.com>
2023-06-14 23:39:18 +0100

    socials

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -7,19 +7,19 @@ import linkedin from "../assets/socials/linkedin.svg";
 const Socials = () => {
   return (
     <div className="flex w-full  h-[2rem] items-center justify-end gap-x-3">
-      <a href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a href="https://www.twitter.com/co_labapes" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
         <img src={youtube} alt="" />
       </a>
-      <a  href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a href="https://www.twitter.com/co_labapes"  className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
         <img src={facebook} alt="" />
       </a>
-      <a  href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a href="https://www.twitter.com/co_labapes" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
         <img src={instagram} alt="" />
       </a>
-      <a  href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a href="https://www.twitter.com/co_labapes" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
         <img src={twitter} alt="" />
       </a>
-      <a  href="" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
+      <a href="https://www.twitter.com/co_labapes" className="border-[#3B8004] border h-[2rem] w-[2rem] rounded-full flex justify-center items-center">
         <img src={linkedin} alt="" />
       </a>
     </div>
