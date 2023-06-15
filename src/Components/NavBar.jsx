459bf388faec428b43bc5516dda04a00f52bf304
himarnoel <olaimarnoel@gmail.com>
2023-06-15 11:23:35 +0100

    nav

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -21,25 +21,35 @@ const NavBar = () => {
         onClick={() => setopensidenav(true)}
         className="text-4xl text-main lg:hidden "
       />
-      <div className="fixed top-0 right-0 bottom-0 h-[100vh] w-full bg-black/[25%] z-30 ">
-        <div
-          className={
-            opensidenav
-              ? "fixed top-0 right-0 bottom-0 h-[100vh] w-[70%] px-4 pt-3 bg-[#F8F8F8] bg-sidenav bg-cover z-30 rounded-l-[20px]"
-              : "fixed top-0 right-0 bottom-0 h-[100vh] w-[70%] px-4 pt-3 bg-[#F8F8F8] bg-sidenav bg-cover z-30 rounded-l-[20px]"
-          }
-        >
-          <div className="flex w-full justify-between items-center ">
-            <img
-              src={sidenavlogo}
-              alt=""
-              className="object-contain w-[10rem]"
-            />
-            <TfiClose
-              onClick={() => setopensidenav(false)}
-              className="text-2xl text-[#999999]"
-            />
-          </div>
+      <div
+        className={
+          opensidenav
+            ? "fixed top-0 right-0 bottom-0 h-[100vh] w-full bg-black/[25%] z-30 "
+            : "hidden"
+        }
+      ></div>
+      <div
+        className={
+          opensidenav
+            ? "fixed top-0 right-0 duration-500 ease-in-out bottom-0 h-[100vh] w-[75%] px-5 pt-3 bg-[#F8F8F8] bg-sidenav bg-cover z-30 rounded-l-[20px]"
+            : "fixed top-0 right-[-100%] duration-500 ease-in-out  bottom-0 h-[100vh] w-[70%] px-4 pt-4 bg-[#F8F8F8] bg-sidenav bg-cover z-30 rounded-l-[20px]"
+        }
+      >
+        <div className="flex w-full justify-between items-center ">
+          <img src={sidenavlogo} alt="" className="object-contain w-[12rem]" />
+          <TfiClose
+            onClick={() => setopensidenav(false)}
+            className="text-2xl text-[#999999]"
+          />
+        </div>
+
+        <div className="flex flex-col mt-16 gap-y-4">
+          <button className="grad h-[3rem] w-full rounded-xl text-white font-semibold">
+            Signup
+          </button>
+          <button className=" h-[3rem] w-full rounded-xl text-main font-semibold">
+            Signin
+          </button>
         </div>
       </div>
     </nav>
