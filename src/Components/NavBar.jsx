38b50068c4c2919fe58e7e9fae39f9f1fa2450ee
himarnoel <olaimarnoel@gmail.com>
2023-06-15 10:58:05 +0100

    tailwind

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiMenuAltRight } from "react-icons/bi";
 import logo from "../assets/logo.svg";
+import sidenavlogo from "../assets/sidenavlogo.png";
+import { TfiClose } from "react-icons/tfi";
 import "../App.css";
 const NavBar = () => {
+  const [opensidenav, setopensidenav] = useState(false);
   return (
     <nav className="flex justify-between  md:pt-8 pt-3">
       <img src={logo} alt="" className="object-contain" />
@@ -14,10 +17,23 @@ const NavBar = () => {
           Sign up
         </button>
       </div>
-      <BiMenuAltRight className="text-4xl text-main lg:hidden " />
+      <BiMenuAltRight
+        onClick={() => setopensidenav(true)}
+        className="text-4xl text-main lg:hidden "
+      />
       <div className="fixed top-0 right-0 bottom-0 h-[100vh] w-full bg-black/[25%] z-30 ">
-        <div className="fixed top-0 right-0 bottom-0 h-[100vh] w-[58%] bg-[#F8F8F8] z-30 rounded-l-[20px]">
-          <div className="h-[20rem] w-[20rem] rounded-full bg-[#054E124D]/[30%] t absolute blur-[500px]  "></div>
+        <div className="fixed top-0 right-0 bottom-0 h-[100vh] w-[70%] px-4 pt-3 bg-[#F8F8F8] bg-sidenav bg-cover z-30 rounded-l-[20px]">
+          <div className="flex w-full justify-between items-center ">
+            <img
+              src={sidenavlogo}
+              alt=""
+              className="object-contain w-[10rem]"
+            />
+            <TfiClose
+              onClick={() => setopensidenav(false)}
+              className="text-2xl text-[#999999]"
+            />
+          </div>
         </div>
       </div>
     </nav>
