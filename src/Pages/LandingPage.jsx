5f492016bf82915449eb5fd17b625dbf22047381
himarnoel <olaimarnoel@gmail.com>
2023-06-16 10:04:34 +0100

    done with background

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -3,22 +3,12 @@ import NavBar from "../Components/NavBar";
 import Hero from "./../Components/Hero";
 import image from "../assets/hero.jpeg";
 import Socials from "./../Components/Socials";
-import bglgscreen from "../assets/hero.jpeg";
-import bgscreen from "../assets/heroMobile.jpeg";
+import Background from "./../Components/Background";
 
 const LandingPage = () => {
   return (
     <div className="px-4  sm:px-20 lg:px-20 min-h-[100%] lg:h-screen w-full  bg-no-repeat  font-lato   bg-cover">
-      <img
-        src={bglgscreen}
-        alt=""
-        className="fixed min-h-full top-0 left-0 z-[-1] hidden lg:block"
-      />
-      <img
-        src={bgscreen}
-        alt=""
-        className="fixed min-h-full top-0 left-0 z-[-1] lg:hidden"
-      />
+      <Background />
       <NavBar />
       <Hero />
       <div
