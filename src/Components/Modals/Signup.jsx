029bf28f938fff908eec6399efa239342104d913
himarnoel <olaimarnoel@gmail.com>
2023-06-24 17:11:03 +0100

    working on modal

--- src/Components/Modals/Signup.jsx
+++ src/Components/Modals/Signup.jsx
@@ -18,31 +18,35 @@ const Signup = () => {
         <div className="grid grid-cols-2  jus">
           <img src={img1} alt="" className=" z-[1] h-[27rem] " />
           <div className="flex flex-col items-center">
-            <img src={logo} alt="" className="w-[8rem] " />
+            <img src={logo} alt="" className="w-[5.8rem] " />
             <p className="text-[#555555] font-bold text-center">
               Sign up to Get Started
             </p>
-            <form onSubmit="" className="w-full relative" action="">
-              <span>
-                {" "}
-                <Message
-                  set="curved"
-                  primaryColor="#999999"
-                  className="inset-y-14 left-1 absolute "
-                />
-              </span>
-              <div className="mt-4  ">
+            <form
+              onSubmit=""
+              className="w-full relative flex flex-col gap-y-1"
+              action=""
+            >
+              <div className="relative">
+                <span>
+                  {" "}
+                  <Message
+                    set="curved"
+                    primaryColor="#999999"
+                    className="inset-y-10 left-1 absolute "
+                  />
+                </span>
                 <label htmlFor="email" className="block font-bold">
                   Email
                 </label>
                 <input
                   type="email"
                   id="email"
-                  className="w-full py-2 bg-[#FFFFFF] rounded-[5px] pl-8 mt-2 border border-[#999999] focus:outline-none"
+                  className="w-full py-2 bg-[#FFFFFF] rounded-[5px] pl-8 mt-1 border border-[#999999] focus:outline-none"
                   placeholder="johndoe@email.com"
                 />
               </div>
-              <div className="mt-4  relative">
+              <div className="mt-2  relative">
                 <span>
                   <Lock
                     set="curved"
@@ -56,11 +60,36 @@ const Signup = () => {
                 <input
                   type="password"
                   id="password"
-                  className="w-full py-2 bg-[#FFFFFF] rounded-[5px] pl-8 mt-2 border border-[#999999] focus:outline-none"
+                  className="w-full py-2 bg-[#FFFFFF] rounded-[5px] pl-8 mt-1 border border-[#999999] focus:outline-none"
                   placeholder="● ● ● ● ●"
                 />
               </div>
-            </form>
+
+              <div className="flex items-start gap-x-3 mt-2 font-normal text-sm">
+                <input type="checkbox" class="accent-[#3B8004]" />
+                <p className="text-[#999999]">
+                  By registering, you are agreeing with our{" "}
+                  <span className="underline text-[#3B8004]">
+                    Terms of Use{" "}
+                  </span>
+                  and{" "}
+                  <span className="underline text-[#3B8004]">
+                    Privacy Policy
+                  </span>
+                </p>
+              </div>
+              <button className="font-semibold bg-gradient-to-bl from-[#054E12] to-[#3B8004] rounded-[10px] h-[2.6rem] mt-4  text-white ">
+                Verify email
+              </button>
+            </form>{" "}
+            <div className="flex flex-col">
+              <p className="text-[#999999]">Or Continue with</p>
+              <div className="flex">
+                <div className="flex justify-center items-center h-[3rem] w-[3rem] border-2 border-solid border-[#DDDDDD] rounded-[10px]"></div>{" "}
+                <div className="flex justify-center items-center h-[3rem] w-[3rem] border-2 border-solid border-[#DDDDDD] rounded-[10px]"></div>{" "}
+                <div className="flex justify-center items-center h-[3rem] w-[3rem] border-2 border-solid border-[#DDDDDD] rounded-[10px]"></div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
