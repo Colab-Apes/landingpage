243887329a104e2b82181733129adbcfdec914db
himarnoel <olaimarnoel@gmail.com>
2023-06-26 17:14:27 +0100

    added an image

--- src/Components/Modals/OtpModal.jsx
+++ src/Components/Modals/OtpModal.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import img from "../../assets/ModalImages/authbg.png";
-import img1 from "../../assets/ModalImages/img3.png";
+import otpmodal from "../../assets/ModalImages/otpmodal.png";
 import logo from "../../assets/ModalImages/logo.png";
 import { TfiClose } from "react-icons/tfi";
-
 
 const OtpModal = ({ setopenOtpmodalprop, openOtpmodalprop, changeValue }) => {
   return (
@@ -29,7 +28,7 @@ const OtpModal = ({ setopenOtpmodalprop, openOtpmodalprop, changeValue }) => {
       />
       <div className="grid lg:grid-cols-2 gap-x-10  ">
         <img
-          src={img1}
+          src={otpmodal}
           alt=""
           className=" object-cover hidden lg:block lg:h-[30rem] xl:h-[30rem] rounded-2xl"
         />
@@ -48,6 +47,13 @@ const OtpModal = ({ setopenOtpmodalprop, openOtpmodalprop, changeValue }) => {
           <div className="mt-4 text-[#3B8004] text-sm font-semibold">
             Enter 4 - digit code sent to johndoe@email.com
           </div>
+          <button className="font-semibold bg-gradient-to-bl from-[#054E12] to-[#3B8004] w-full rounded-[10px] h-[2.6rem] mt-6 md:mt-8 lg:mt-4  text-white ">
+            Verify email
+          </button>
+          <p className="text-xs">
+            Already have an account?
+            <a className="text-[#054E12]"> Sign In</a>
+          </p>
         </div>
       </div>
     </div>
