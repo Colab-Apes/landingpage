6d144e7366b94b6025416fe6eb34cda4e7cd1ec4
himarnoel <olaimarnoel@gmail.com>
2023-06-29 03:44:28 +0100

    adjusted modal

--- src/Components/Modals/Overlay.jsx
+++ src/Components/Modals/Overlay.jsx
@@ -18,20 +18,19 @@ const Overlay = ({ value, changeValue }) => {
   const [openverifiedmodal, setopenverifiedmodal] = useState(false);
   const [opensignupmodal, setopensignupmodal] = useState(true);
 
-
   return (
     <div
       className={
         value
-          ? "bg-black/[25%] fixed h-screen w-full z-10  px-20 sm:px-12 md:px-20 left-0 flex justify-center items-center font-lato duration-300 ease-in-out "
-          : "bg-black/[25%] fixed h-screen w-full z-[-1] px-20 sm:px-12 md:px-20 left-0 flex justify-center items-center font-lato  duration-300 ease-in-out"
+          ? "bg-black/[25%] fixed h-screen w-full z-10  px-8 sm:px-12 md:px-20 left-0 flex justify-center items-center font-lato duration-300 ease-in-out "
+          : "bg-black/[25%] fixed h-screen w-full z-[-1] px-8 sm:px-12 md:px-20 left-0 flex justify-center items-center font-lato  duration-300 ease-in-out"
       }
     >
       <div
         className={
           value
-            ? "rounded-2xl bg-[#F5FFF7] flex items-center justify-center h-[34rem] w-screen sm:h-[40rem] md:h-[45rem] lg:h-[32rem] xl:h-[34rem]  md:w-[52rem] px-8 md:px-[3rem] opacity-100  relative duration-[800ms] ease-in-out"
-            : "rounded-2xl bg-[#F5FFF7] flex items-center justify-center h-[34rem] w-screen sm:h-[40rem] md:h-[45rem] lg:h-[32rem] xl:h-[34rem]  md:w-[52rem] px-8 md:px-[3rem] opacity-0  relative duration-[100ms] ease-in-out"
+            ? "rounded-2xl bg-[#F5FFF7] flex items-center justify-center h-[32rem] w-full sm:h-[40rem] md:h-[45rem] lg:h-[32rem] xl:h-[34rem]  md:w-[52rem] px-4 sm:px-8 md:px-[3rem] opacity-100  relative duration-[800ms] ease-in-out"
+            : "rounded-2xl bg-[#F5FFF7] flex items-center justify-center h-[32rem] w-full sm:h-[40rem] md:h-[45rem] lg:h-[32rem] xl:h-[34rem]  md:w-[52rem] px-4 sm:px-8 md:px-[3rem] opacity-0  relative duration-[100ms] ease-in-out"
         }
       >
         <img
@@ -46,7 +45,7 @@ const Overlay = ({ value, changeValue }) => {
             setopenverifiedmodal(false);
             setopenOtpmodal(false);
           }}
-          className="text-2xl lg:text-xl text-[#999999] font-bold cursor-pointer absolute right-3 top-3 md:top-8 lg:top-4 md:right-8 lg:right-4"
+          className="text-2xl lg:text-xl text-[#999999] font-bold cursor-pointer absolute right-6 top-6  md:top-8 lg:top-4 md:right-8 lg:right-4"
         />
 
         <Signup
--- src/Components/Modals/Signup.jsx
+++ src/Components/Modals/Signup.jsx
@@ -20,7 +20,9 @@ const Signup = ({
   return (
     <div
       className={
-        opensignupmodalprop ? "grid lg:grid-cols-2 sm:gap-x-10" : "hidden"
+        opensignupmodalprop
+          ? "flex flex-col  w-full lg:grid lg:grid-cols-2 gap-x-10"
+          : "hidden"
       }
     >
       <img
@@ -28,7 +30,7 @@ const Signup = ({
         alt=""
         className=" object-cover hidden lg:block lg:h-[30rem] xl:h-[30rem] rounded-2xl"
       />
-      <div className="flex flex-col items-center  sm:w-[19rem] h-[30rem] md:w-full md:h-full">
+      <div className="flex flex-col w-full items-center sm:w-[19rem] h-[30rem] md:w-full md:h-full">
         <img
           src={logo}
           alt=""
