e7272d89031f5900db7100ac8e550d36fc84990f
himarnoel <olaimarnoel@gmail.com>
2023-06-29 23:21:36 +0100

    doen

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -21,15 +21,15 @@ const Hero = () => {
 
         {/* Animated Text */}
         <TypeAnimation
-          className="text-[1.5rem] leading-[1.6] text-left lg:leading-[3rem] w-full sm:w-[33rem] md:w-[40rem] lg:w-[30rem] h-[8rem] sm:h-[12.6rem] xl:leading-[3.6rem] sm:text-[1.9rem] md:text-4xl lg:text-[2rem] xl:text-[2.5rem] text-main mt-4 font-bold"
+          className="text-[1.5rem] leading-[1.7] text-left lg:leading-[1.5] w-full sm:w-[33rem] md:w-[40rem] lg:w-[30rem] h-[8rem] sm:h-[12.6rem]  md:h-[11rem] xl:leading-[1.5] sm:text-[1.9rem] md:text-4xl lg:text-[2rem] xl:text-[2.2rem] text-main mt-4 lg:mt-2 font-bold"
           sequence={[
             // Same substring at the start will only be typed once, initially
-            "Unlock Your Digital Team: Find, Collaborate, and Build the Future Together",
-            10000,
-            "Unlock Your Digital Dream Team: Find, Collaborate, and Soar Together",
-            10000,
-            "Unleash Your Digital Vision with the Perfect Dream Team",
-            10000,
+            // "Unlock Your Digital Team: Find, Collaborate, and Build the Future Together",
+            // 10000,
+            // "Unlock Your Digital Dream Team: Find, Collaborate, and Soar Together",
+            // 10000,
+            // "Unleash Your Digital Vision with the Perfect Dream Team",
+            // 10000,
             "Empowering Digital Entrepreneurs to Build their Dream Team for Success",
             10000,
             "Unleash Your Digital Vision. Find Your Dream Team Today!",
@@ -43,11 +43,11 @@ const Hero = () => {
         <img
           src={heroimage}
           alt=""
-          className="w-[80%] sm:w-[27rem] object-contain lg:hidden mx-auto lg:mx-0  mt-8 sm:mt-4"
+          className="w-[80%] sm:w-[27rem] object-contain lg:hidden mx-auto lg:mx-0  mt-8 sm:mt-4 "
           loading="lazy"
         />
 
-        <div className="flex flex-col w-full mt-8  sm:mt-10 justify-between">
+        <div className="flex flex-col w-full mt-8  sm:mt-10 justify-between lg:mt-2">
           {/* Waitlist Form */}
           <p className="text-[#054E12] text-xl font-lato font-medium">
             Join the wait list
