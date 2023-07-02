75f7c388b6e6a82a5ca514097af113f7e9b66bd6
himarnoel <olaimarnoel@gmail.com>
2023-07-02 21:06:18 +0100

    done with modal

--- src/Components/OtpInputField.jsx
+++ src/Components/OtpInputField.jsx
@@ -23,16 +23,20 @@ const OTPInputField = ({ length }) => {
   return (
     <div className="flex justify-center items-center">
       {otp.map((value, index) => (
-        <input
-          key={index}
-          type="text"
-          className="w-12 h-12 border border-gray-300 rounded-md text-center mx-2 focus:outline-none focus:ring focus:border-blue-300"
-          value={value}
-          maxLength="1"
-          onChange={(e) => handleChange(e, index)}
-          onKeyDown={(e) => handleKeyDown(e, index)}
-          ref={(ref) => (inputRefs.current[index] = ref)}
-        />
+        <div className="relative" key={index}>
+          <input
+            type="text"
+            className="w-16 h-11 border-[2px] rounded-md text-center mx-2 focus:outline-none border-[#999] focus:border-[#4EAF00]"
+            value={value}
+            maxLength="1"
+            onChange={(e) => handleChange(e, index)}
+            onKeyDown={(e) => handleKeyDown(e, index)}
+            ref={(ref) => (inputRefs.current[index] = ref)}
+          />
+          {!value && (
+            <div className="absolute left-[50%]  right-[50%] w-[2rem]  translate-x-[-50%]  translate-y-[-50%] bottom-2 bg-[#999999] h-[1.2px] "></div>
+          )}
+        </div>
       ))}
     </div>
   );
