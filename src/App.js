ef2e2dc04013312f33d4c9352a72b45f9799dfc8
himarnoel <olaimarnoel@gmail.com>
2023-07-03 22:11:25 +0100

    started routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LandingPage from "./Pages/LandingPage";
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <>
-      <LandingPage />
-      {/* <Signup /> */}
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </>
   );
 }
