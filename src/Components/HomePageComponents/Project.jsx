276ee740900a4914747427bb2260937a5ea5c3a3
himarnoel <olaimarnoel@gmail.com>
2023-07-04 16:11:30 +0100

    still working ion project

--- src/Components/HomePageComponents/Project.jsx
+++ src/Components/HomePageComponents/Project.jsx
@@ -3,22 +3,22 @@ import img from "../../assets/projectimages/1.png";
 import bag from "../../assets/homepageicons/images/bag.svg";
 import group from "../../assets/homepageicons/images/user.svg";
 import info from "../../assets/homepageicons/images/info.svg";
-const Project = () => {
+const Project = ({ image }) => {
   return (
-    <div className="w-[15rem]  shadow-xl h-[25rem] rounded-[30px]">
-      <img src={img} alt="" className="object-cover w-[15rem] " />
-      <div className="px-4 w-full  h-[12rem] rounded-b-[30px] flex flex-col justify-evenly">
-        <div className="flex">
+    <div className="w-[15rem]  font-lato flex flex-col  shadow-[15px_15px_50px_0px_rgba(5,78,18,0.10);] hover:shadow-sm cursor-pointer ease-out hover:duration-200 h-[24rem] rounded-[30px] ">
+      <img src={image} alt="" className="object-cover w-[15rem] " />
+      <div className="px-4 w-full text-xs flex-1 rounded-b-[30px] flex flex-col justify-evenly">
+        <div className="flex gap-x-3">
           <img src={bag} alt="" className="object-contain w-[1rem]" />
-          <p>Mobile game</p>
+          <p className="font-bold">Mobile game</p>
         </div>
-        <div className="flex">
+        <div className="flex gap-x-3">
           <img src={group} alt="" className="object-contain w-[1rem]" />
-          <p>Multiple owners</p>
+          <p className="text-[#999999]">Multiple owners</p>
         </div>
-        <div className="flex text-xs">
+        <div className="flex text-xs gap-x-3 items-start">
           <img src={info} alt="" className="object-contain w-[1rem]" />
-          <p>
+          <p className="text-[#999999]">
             Lorem ipsum dolor sit amet consectetur. Justo feugiat pharetra eu eu
             enim...
           </p>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import NavBarComp from "../Components/HomePageComponents/NavBarComp";
 import Project from "../Components/HomePageComponents/Project";
+import img1 from "../assets/projectimages/1.png";
+import img2 from "../assets/projectimages/2.png";
+import img3 from "../assets/projectimages/3.png";
+import img4 from "../assets/projectimages/4.png";
 const Home = () => {
   return (
     <div className=" font-lato ">
@@ -8,7 +12,7 @@ const Home = () => {
       <div className="px-24 mt-4">
         {" "}
         <p className="text-left">
-          <p className="text-2xl">
+          <p className="text-3xl font-semibold">
             {" "}
             Discover Amazing Projects From Around The World
           </p>
@@ -17,7 +21,19 @@ const Home = () => {
             Story
           </p>
         </p>
-        <Project />
+        <div className="grid lg:grid-cols-4 mt-5 gap-y-4 justify-center place-items-center">
+          <Project image={img1} />
+          <Project image={img2} />
+          <Project image={img3} />
+          <Project image={img4} />
+          <Project image={img1} />
+          <Project image={img2} />
+          <Project image={img3} />
+          <Project image={img4} /> <Project image={img1} />
+          <Project image={img2} />
+          <Project image={img3} />
+          <Project image={img4} />
+        </div>
       </div>
     </div>
   );
