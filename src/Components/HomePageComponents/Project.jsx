e5327fadd035e5b5dd18d768a72ac01c0849c52f
himarnoel <olaimarnoel@gmail.com>
2023-07-04 15:50:39 +0100

    removed bugs

--- src/Components/HomePageComponents/Project.jsx
+++ src/Components/HomePageComponents/Project.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import img from "../../assets/projectimages/1.png";
 import bag from "../../assets/homepageicons/images/bag.svg";
 import group from "../../assets/homepageicons/images/user.svg";
-import info from "../../assets/homepageicons/images/use";
+import info from "../../assets/homepageicons/images/info.svg";
 const Project = () => {
   return (
-    <div className="w-[15rem]  bg-red-300 shadow-xl h-[25rem] rounded-[30px]">
+    <div className="w-[15rem]  shadow-xl h-[25rem] rounded-[30px]">
       <img src={img} alt="" className="object-cover w-[15rem] " />
-      <div className="px-4 w-full bg-red-500 h-[12rem] rounded-b-[30px] flex flex-col justify-evenly">
+      <div className="px-4 w-full  h-[12rem] rounded-b-[30px] flex flex-col justify-evenly">
         <div className="flex">
           <img src={bag} alt="" className="object-contain w-[1rem]" />
           <p>Mobile game</p>
@@ -17,7 +17,7 @@ const Project = () => {
           <p>Multiple owners</p>
         </div>
         <div className="flex text-xs">
-          <img src={bag} alt="" className="object-contain w-[1rem]" />
+          <img src={info} alt="" className="object-contain w-[1rem]" />
           <p>
             Lorem ipsum dolor sit amet consectetur. Justo feugiat pharetra eu eu
             enim...
