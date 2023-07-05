66b563514397c154bde478b9cd4a5b7aeaf4618b
himarnoel <olaimarnoel@gmail.com>
2023-07-05 23:06:04 +0100

    icons added

--- src/Components/HomePageComponents/MobileNavComp.jsx
+++ src/Components/HomePageComponents/MobileNavComp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiMenuAltRight } from "react-icons/bi";
 import logo from "../../assets/logo.svg";
 // import sidenavlogo from "../assets/sidenavlogo.png";
@@ -6,6 +6,8 @@ import { TfiClose } from "react-icons/tfi";
 import "../../App.css";
 
 const MobileNavComp = () => {
+  const [opensidenav, setopensidenav] = useState(false);
+
   return (
     <div>
       <nav className="flex lg:hidden px-4 justify-between fixed bg-white top-0 w-screen items-center min-h-[10vh] sm:pt-8 pt-3 lg:pt-0">
@@ -19,9 +21,52 @@ const MobileNavComp = () => {
         </a>
 
         <BiMenuAltRight
-          // onClick={() => setopensidenav(true)}
+          onClick={() => setopensidenav(true)}
           className="text-5xl md:text-5xl text-main lg:hidden"
         />
+        {/* Background Overlay */}
+        <div
+          onClick={() => setopensidenav(false)}
+          className={
+            opensidenav
+              ? "fixed top-0 right-0 bottom-0 h-[100vh] w-full bg-black/[25%] z-30 lg:hidden"
+              : "hidden lg:hidden"
+          }
+        ></div>
+
+        {/* Side Navigation */}
+        <div
+          className={
+            opensidenav
+              ? "fixed top-0 right-0 duration-500 ease-in-out bottom-0 h-[100vh] w-[75%] sm:w-[60%] px-5 sm:px-16 md:px-20 pt-3 sm:pt-10 bg-[#F8F8F8] bg-sidenav bg-cover z-30 rounded-l-[20px] lg:hidden"
+              : "fixed top-0 right-[-100%] duration-500 ease-in-out bottom-0 h-[100vh] w-[75%] sm:w-[60%] px-5 sm:px-16 md:px-20 pt-3 sm:pt-10 bg-[#F8F8F8] bg-sidenav bg-cover z-30 rounded-l-[20px] lg:hidden"
+          }
+        >
+          <div className="flex w-full justify-between items-center ">
+            <img src={logo} alt="" className="object-cover w-[12rem] " />
+            <TfiClose
+              onClick={() => setopensidenav(false)}
+              className="text-2xl text-[#999999]"
+            />
+          </div>
+
+          <div className="flex flex-col mt-16 gap-y-10">
+            {/* Signup Button */}
+            <button
+              onClick={() => {
+                setopensidenav(false);
+              }}
+              className="h-[3rem] w-full rounded-xl gradnav text-white font-semibold"
+            >
+              Signup
+            </button>
+
+            {/* Signin Button */}
+            {/* <button className="h-[3rem] w-full rounded-xl text-main font-semibold">
+            Signin
+          </button> */}
+          </div>
+        </div>
       </nav>
     </div>
   );
