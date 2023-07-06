2f19bd2fcea6df63fc716646178ea56270d580e3
himarnoel <olaimarnoel@gmail.com>
2023-07-07 00:22:12 +0100

    editing the work

--- src/Components/HomePageComponents/Project.jsx
+++ src/Components/HomePageComponents/Project.jsx
@@ -7,11 +7,11 @@ import hand from "../../assets/homepageicons/images/group-3.svg";
 import like from "../../assets/homepageicons/images/like.svg";
 const Project = ({ image, name }) => {
   return (
-    <div className=" w-full sm:w-[20rem] lg:w-[15rem]   font-lato flex flex-col  shadow-[15px_15px_50px_0px_rgba(5,78,18,0.10)] hover:shadow-sm cursor-pointer ease-out hover:duration-200 min-h-[34rem] sm:min-h-[35rem] md:min-h-[35rem] lg:min-h-[30rem] rounded-[30px] ">
+    <div className=" w-full sm:w-[20rem] lg:w-[15rem] xl:w-[18rem]   font-lato flex flex-col  shadow-[15px_15px_50px_0px_rgba(5,78,18,0.10)] hover:shadow-sm cursor-pointer ease-out hover:duration-200 min-h-[34rem] sm:min-h-[35rem] md:min-h-[35rem] lg:min-h-[30rem] rounded-[30px] ">
       <img
         src={image}
         alt=""
-        className="object-cover w-full sm:w-[20rem] lg:w-[15rem] "
+        className="object-cover w-full sm:w-[20rem] lg:w-[15rem] xl:w-[18rem] "
       />
       <div className="px-4 sm:px-4 lg:px-4 w-full text-lg lg:text-xs flex-1 rounded-b-[30px] flex flex-col justify-evenly">
         <div className="flex flex-col text-base sm:text-2xl lg:text-base gap-y-3 sm:gap-y-6  lg:gap-y-3 text-[#333]">
@@ -29,7 +29,7 @@ const Project = ({ image, name }) => {
               alt=""
               className="object-contain  sm:w-[1.9rem]  w-[1rem] lg:w-[1rem]"
             />
-            <p className="text-[#999999] text-base">Multiple owners</p>
+            <p className="text-[#999999] sm:text-base">Multiple owners</p>
           </div>
           <div className="flex  lg:text-xs gap-x-3 items-start">
             <img
@@ -37,7 +37,7 @@ const Project = ({ image, name }) => {
               alt=""
               className="object-contain  sm:w-[1.9rem]  w-[1rem] lg:w-[1rem]"
             />
-            <p className="text-[#999999] text-sm ">
+            <p className="text-[#999999] sm:text-base ">
               Lorem ipsum dolor sit amet consectetur. Justo feugiat pharetra eu
               eu enim...
             </p>
