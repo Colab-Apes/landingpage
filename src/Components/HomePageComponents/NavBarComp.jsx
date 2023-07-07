6edd7bad9d34d8d196b4fd9ad29f1c77ffe3b237
himarnoel <olaimarnoel@gmail.com>
2023-07-07 01:08:30 +0100

    nav

--- src/Components/HomePageComponents/NavBarComp.jsx
+++ src/Components/HomePageComponents/NavBarComp.jsx
@@ -5,10 +5,18 @@ import icon from "../../assets/homepageicons/images/group-50.svg";
 import avatar from "../../assets/homepageicons/images/avatar.png";
 import { BsChevronDown } from "react-icons/bs";
 import MobileNavComp from "./MobileNavComp";
+import { useState } from "react";
 const NavBarComp = () => {
+  const [tabs, settabs] = useState([
+    "Home",
+    "Learning  Pool",
+    "Create Project",
+  ]);
+  const [checkindex, setcheckindex] = useState(0);
+
   return (
     <div>
-      <nav className="font-lato lg:px-6 xl:px-10 bg-white z-20 hidden  lg:flex justify-between items-center shadow-md  fixed top-0  h-[8rem] w-full ">
+      <nav className="font-lato lg:px-6 xl:px-10 bg-white z-20 hidden  lg:flex justify-between items-center shadow-md  fixed top-0  h-[9rem] w-full ">
         <a href="/">
           <img
             src={logo}
@@ -16,14 +24,22 @@ const NavBarComp = () => {
             className="object-contain w-[10rem] md:w-[15rem] lg:w-[14rem]"
           />
         </a>
-        <div className=" z-[20]  flex items-center gap-x-20 text-xl text-center mt-4">
-          <p className=" border-b-4 border-b-[#054E12] w-[4rem] pb-4 cursor-pointer">
-            Home
-          </p>
-          <p className="text-[#33333380]  pb-4 cursor-pointer">Learning pool</p>{" "}
-          <p className=" text-[#33333380]  pb-4 cursor-pointer">
-            Create project
-          </p>
+        <div className=" z-[20]  flex items-center w-[40vw] gap-x-20 text-xl text-center mt-8">
+          {tabs.map((tab, index) => (
+            <p
+              onClick={() => setcheckindex(index)}
+              key={index}
+              className={
+                index === checkindex
+                  ? `border-b-2  border-b-[#054E12] ${
+                      index == 0 ? "w-[8rem]" : "w-[8rem]"
+                    } pb-10 cursor-pointer`
+                  : "text-[#33333380]  pb-10 cursor-pointer border-b-2 w-[8rem]  border-b-white"
+              }
+            >
+              {tab}
+            </p>
+          ))}
         </div>
         <div className="flex items-center gap-x-6">
           <img src={icon} alt="" className="w-[4rem] object-contain" />
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,14 +10,14 @@ const Home = () => {
   return (
     <div className=" font-lato  bg-white ">
       <NavBarComp />
-      <div className=" px-8 sm:px-10 lg:px-6 xl:px-10 mt-20 sm:mt-32  lg:mt-[9rem] xl:mt-[10rem]   ">
+      <div className=" px-8 sm:px-10 lg:px-6 xl:px-10 mt-20 sm:mt-32  lg:mt-[10rem] xl:mt-[12rem]   ">
         <div className="h-[5rem] sm:text-2xl flex items-center justify-evenly w-full mx-auto bg-white rounded-[10px] shad lg:hidden">
           <p className=" border-b-4 font-bold border-b-[#054E12] pb-4">Home</p>
           <p className=" pb-4 text-[#333]/[50%]">Learning Pool</p>
           <p className=" pb-4 text-[#333]/[50%]">Create project</p>
         </div>
-        <p className="text-left font-inter text-[#333] mt-4 lg:mt-0">
-          <p className=" text-2xl sm:text-3xl lg:text-4xl xl:text-[2.8rem] font-semibold">
+        <p className="text-left font-inter text-[#333] mt-4 ">
+          <p className=" text-2xl sm:text-3xl lg:text-4xl xl:text-[2.5rem] font-semibold">
             Discover Amazing Projects From Around The World
           </p>
           <p className="text-left mt-4 text-sm sm:text-base lg:mt-2 xl:mt-4 ">
@@ -25,19 +25,19 @@ const Home = () => {
             Story
           </p>
         </p>
-        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 sm:mt-20 lg:mt-6 xl:mt-8 gap-y-10 sm:gap-y-20 md:gap-x-20 sm:gap-x-10 md:gap-y-32 lg:gap-x-2 lg:gap-y-10  mx-auto justify-center w-full place-items-center">
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-4 sm:mt-20 lg:mt-8 xl:mt-10 gap-y-10 sm:gap-y-20 md:gap-x-20 sm:gap-x-10 md:gap-y-32 lg:gap-x-2 lg:gap-y-10  mx-auto justify-center w-full place-items-center">
           <Project image={img1} name="Mobile game" />
           <Project image={img2} name="Product design" />
           <Project image={img3} name="Video game" />
           <Project image={img4} name="data analyst" />
-          {/* <Project image={img1} name="Mobile game" />
+          <Project image={img1} name="Mobile game" />
           <Project image={img2} name="Product design" />
           <Project image={img3} name="Video game" />
           <Project image={img4} name="data analyst" />{" "}
           <Project image={img1} name="Mobile game" />
           <Project image={img2} name="Product design" />
           <Project image={img3} name="Video game" />
-          <Project image={img4} name="data analyst" /> */}
+          <Project image={img4} name="data analyst" />
         </div>
         <div className="w-full flex justify-center items-center  mt-10 mb-10">
           {" "}
