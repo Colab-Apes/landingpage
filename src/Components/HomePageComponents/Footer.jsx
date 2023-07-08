388984b33a8300eb69de1ef6affe438180982bdd
himarnoel <olaimarnoel@gmail.com>
2023-07-08 22:15:22 +0100

    ccpl

--- src/Components/HomePageComponents/Footer.jsx
+++ src/Components/HomePageComponents/Footer.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 import "../../App.css";
+import ape from "../../assets/homepageicons/footerimages/apehead.png";
 const Footer = () => {
   return (
     <div>
-      <footer className=" h-[25rem] w-full rounded-[15px]  grad"></footer>
+      <footer className=" h-[25rem] w-full rounded-[15px]  grad relative">
+        <img
+          src={ape}
+          alt=""
+          className="top-0 absolute object-contain w-[21.58rem] z-[1]"
+        />
+        <div className="grid grid-cols-4">
+          <div className="flex flex-col items-center">
+            <p className="ss">Co_lab Apes</p>
+          </div>
+          <div className="flex flex-col items-center"></div>
+          <div className="flex flex-col items-center"></div>
+          <div className="flex flex-col items-center"></div>
+        </div>
+      </footer>
     </div>
   );
 };
