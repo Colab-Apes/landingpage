6fa0ebddf6eee8d07915bcc06e83ee180a711cf1
himarnoel <olaimarnoel@gmail.com>
2023-07-19 15:04:58 +0100

    working on footer

--- src/Components/HomePageComponents/Footer.jsx
+++ src/Components/HomePageComponents/Footer.jsx
@@ -8,18 +8,26 @@ const Footer = () => {
         <img
           src={ape}
           alt=""
-          className="top-0 absolute object-contain w-[21.58rem] z-[1]"
+          className="top-0 absolute object-contain w-[21.58rem] rounded-[15px] left-0 z-[1] "
         />
-        <div className="grid grid-cols-4 text-white pt-20">
-          <div className="flex flex-col items-center z-[2]">
-            <p className="text-xl font-bold">Co_lab Apes</p>
-            <p className="ss">About Us</p>
+        <div className="grid grid-cols-4  text-white pt-10">
+          <div className="flex flex-col w-fit mx-auto  text-left z-[3] gap-y-8">
+            <p className="text-lg font-bold ">Co_lab Apes</p>
+            <p className=" mt-10">About Us</p>
             <p className="ss">About Us</p>
             <p className="ss">Privacy policy</p>
+            <p className="ss">Terms of Use</p>{" "}
+            <p className="ss">Legal</p>
           </div>
-          <div className="flex flex-col items-center"> <p className="ss">Features</p></div>
-          <div className="flex flex-col items-center"> <p className="ss">Features</p></div>
-          <div className="flex flex-col items-center"> <p className="ss">Features</p></div>
+          <div className="flex flex-col items-center">
+            <p className="text-lg font-bold">Features</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-lg font-bold">Learning pool</p>
+          </div>
+          <div className="flex flex-col items-center">
+            <p className="text-lg font-bold">Features</p>
+          </div>
         </div>
       </footer>
     </div>
