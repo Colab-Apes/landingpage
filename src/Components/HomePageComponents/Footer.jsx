703a3528b7b1038d186573c1240f9fb6787d7aca
himarnoel <olaimarnoel@gmail.com>
2023-07-20 01:10:36 +0100

    removed bug

--- src/Components/HomePageComponents/Footer.jsx
+++ src/Components/HomePageComponents/Footer.jsx
@@ -14,30 +14,30 @@ const Footer = () => {
           className="bottom-0 lg:top-0 absolute object-contain w-[26rem] rounded-l-[15px] left-0 z-[1] "
         />
         <div className="grid grid-cols-2 lg:grid-cols-4 z-[10] text-white pt-10 h-full gap-x-8 gap-y-10 lg:gap-y-0 ">
-          <div className="flex flex-col w-fit mx-auto   text-sm text-left z-[3] gap-y-5">
+          <div className="flex flex-col w-fit mx-auto   text-left z-[3] gap-y-5">
             <p className="text-lg font-bold ">Co_lab Apes</p>
             <p className=" mt-5">About Us</p>
-            <p className="ss">About Us</p>
-            <p className="ss">Privacy policy</p>
-            <p className="ss">Terms of Use</p> <p className="ss">Legal</p>
+            <p className="">About Us</p>
+            <p className="">Privacy policy</p>
+            <p className="">Terms of Use</p> <p className="">Legal</p>
           </div>
           <div className="flex flex-col w-fit mx-auto   text-sm text-left z-[3] gap-y-5">
             <p className="text-lg font-bold">Features</p>
             <p className=" mt-5">Home</p>
-            <p className="ss">Top 10 projects</p>
-            <p className="ss">Learning pool</p> <p className="ss">Join </p>
-            <p className="ss">Create project</p>
+            <p className="">Top 10 projects</p>
+            <p className="">Learning pool</p> <p className="">Join </p>
+            <p className="">Create project</p>
           </div>
           <div className="flex flex-col w-fit mx-auto   text-sm text-left z-[3] gap-y-5">
             <p className="text-lg font-bold">Learning pool</p>
             <p className="mt-5">Analytics</p>
-            <p className="ss">Artificial intelligence</p>{" "}
-            <p className="ss">Animation </p>
-            <p className="ss">Coding</p>
-            <p className="ss">Content writing</p>
-            <p className="ss">Data science</p>
-            <p className="ss">Video editting</p>
-            <p className="ss">3D modelling</p>
+            <p className="">Artificial intelligence</p>{" "}
+            <p className="">Animation </p>
+            <p className="">Coding</p>
+            <p className="">Content writing</p>
+            <p className="">Data science</p>
+            <p className="">Video editting</p>
+            <p className="">3D modelling</p>
           </div>
           <div className="flex flex-col items-center z-[3] text-sm">
             <img src={logo} alt="" className="z-[2]" />
@@ -47,14 +47,14 @@ const Footer = () => {
               Contact Us
             </button>
           </div>
-          <div className="h-[0.1rem] rounded w-full bg-white z-[3] col-span-2 lg:col-span-4 "></div>
-          <div className="col-span-2 lg:col-span-4 flex justify-center items-center">
+          <div className="h-[0.1rem] rounded w-full bg-white z-[3] col-span-2 lg:col-span-4 lg:mt-6 "></div>
+          <div className="col-span-2 lg:col-span-4 flex justify-center items-center lg:mt-4 z-[3] mb-2">
             <div className="flex items-center">
               {" "}
               <div className="text-2xl">
                 <CgCopyright />
               </div>
-              <div className="ss">2023, all rights reserved</div>
+              <div className="">2023, all rights reserved</div>
             </div>
           </div>
         </div>
