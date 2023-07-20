54e09544983ad2ca5b55edae0334f1bac1bbbc21
himarnoel <olaimarnoel@gmail.com>
2023-07-20 19:07:51 +0100

    done

--- src/Pages/CreateProject.jsx
+++ src/Pages/CreateProject.jsx
@@ -4,10 +4,7 @@ const CreateProject = () => {
   return (
     <div className="font-lato">
       <div className="w-full shadow-[-5px_15px_50px_0px_rgba(5,78,18,0.05)] h-[32rem] bg-fuchsia-300 rounded-[30px] px-10">
-        <p className="text-[#333] text-[40px] font-bold mt-5">
-          Create new project
-        </p>
-        
+        <p className="text-[#333] text-[40px] font-bold">Create new project</p>
       </div>
       <div className="w-full flex justify-center items-center  mt-10 mb-10"></div>
     </div>
