1f8b6625cac6e12b2e16900da072606f20e6a630
himarnoel <olaimarnoel@gmail.com>
2023-07-20 15:53:52 +0100

    done

--- src/Components/HomePageComponents/Learn.jsx
+++ src/Components/HomePageComponents/Learn.jsx
@@ -4,41 +4,38 @@ import book from "../../assets/Learnimages/icons/book.svg";
 import group from "../../assets/Learnimages/icons/group.svg";
 const Learn = ({ image, name }) => {
   return (
-    <div className=" w-full  font-lato flex flex-col  shadow-[15px_15px_50px_0px_rgba(5,78,18,0.10)] hover:shadow-sm cursor-pointer ease-out hover:duration-200 min-h-[34rem] sm:min-h-[35rem] md:min-h-[35rem] lg:min-h-[30rem] xl:min-h-[34rem] rounded-[30px] ">
+    <div className=" w-full  font-lato flex flex-col  shadow-[15px_15px_50px_0px_rgba(5,78,18,0.10)] hover:shadow-sm cursor-pointer ease-out hover:duration-200 min-h-[34rem] sm:min-h-[35rem] md:min-h-[35rem] lg:min-h-[30rem] xl:min-h-[26rem] rounded-[30px] ">
       <img src={image} alt="" className="object-cover w-full " />
-      <div className="px-4 sm:px-4 lg:px-4 w-full text-lg lg:text-xs flex-1 rounded-b-[30px] flex flex-col justify-evenly">
-        <div className="flex flex-col text-base sm:text-2xl lg:text-base gap-y-3 sm:gap-y-6  lg:gap-y-3 text-[#333]">
-          <div className="flex gap-x-3 text-xl sm:text-3xl  lg:text-base">
+
+      <div className="flex flex-col text-base sm:text-2xl lg:text-base gap-y-3 px-4 sm:gap-y-6  lg:gap-y-4 text-[#333]">
+        <div className="flex gap-x-3 text-xl sm:text-3xl  lg:text-base pt-4">
+          <img
+            src={pen}
+            alt=""
+            className="object-contain sm:w-[1.9rem] lg:w-[1rem]"
+          />
+          <p className="font-bold">{name}</p>
+        </div>
+        <div className="flex gap-x-3 text-xl sm:text-3xl  lg:text-base">
+          <img
+            src={book}
+            alt=""
+            className="object-contain sm:w-[1.9rem] lg:w-[1rem]"
+          />
+          <p className="text-[#999999] text-sm">310,000 resources</p>
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="flex gap-x-3">
             <img
-              src={pen}
+              src={group}
               alt=""
-              className="object-contain sm:w-[1.9rem] lg:w-[1rem]"
+              className="object-contain  sm:w-[1.9rem]  w-[1rem] lg:w-[1rem]"
             />
-            <p className="font-bold">{name}</p>
+            <p className="text-[#999999] text-xs">10K+ members</p>
           </div>
-          <div className="flex gap-x-3 text-xl sm:text-3xl  lg:text-base">
-            <img
-              src={book}
-              alt=""
-              className="object-contain sm:w-[1.9rem] lg:w-[1rem]"
-            />
-            <p className="font-bold">310,000 resources</p>
-          </div>
-          <div className="flex justify-between items-center">
-            <div className="flex gap-x-3">
-              <img
-                src={group}
-                alt=""
-                className="object-contain  sm:w-[1.9rem]  w-[1rem] lg:w-[1rem]"
-              />
-              <p className="text-[#999999] sm:text-base lg:text-sm">
-                10K+ members
-              </p>
-            </div>
-            <button className="h-[2rem] w-[8rem] sm:h-[2rem] sm:w-[8rem] lg:h-[2rem] lg:w-[6rem] border border-solid border-[#999] rounded-[15px] hover:text-white hover:bg-gradient-to-bl hover:from-[#054E12] hover:to-[#3B8004]">
-              Join project
-            </button>
-          </div>
+          <button className="h-[2rem] w-[8rem] sm:h-[2rem] sm:w-[8rem] lg:h-[2rem] lg:w-[6rem] border border-solid border-[#999] text-[#999] text-sm rounded-[15px] hover:text-white hover:bg-gradient-to-bl hover:from-[#054E12] hover:to-[#3B8004]">
+            Join project
+          </button>
         </div>
       </div>
     </div>
