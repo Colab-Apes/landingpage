db9b42092364ae5399c3863b1ea672d86bf1cbe8
himarnoel <olaimarnoel@gmail.com>
2023-07-20 18:44:16 +0100

    moved

--- src/Pages/CreateProject.jsx
+++ src/Pages/CreateProject.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const CreateProject = () => {
   return (
-    <div>
-      CreateProject
+    <div className="font-lato">
+      <div className="w-full shadow-[-5px_15px_50px_0px_rgba(5,78,18,0.05)] h-[32rem] bg-fuchsia-300 rounded-[30px] px-10">
+        <p className="text-[#333] text-[40px] font-bold mt-5">
+          Create new project
+        </p>
+        
+      </div>
       <div className="w-full flex justify-center items-center  mt-10 mb-10"></div>
     </div>
   );
