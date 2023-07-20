e7bbd36a9a61116a61e66842c759460736271ed7
himarnoel <olaimarnoel@gmail.com>
2023-07-20 19:49:10 +0100

    created a ui

--- src/Pages/CreateProject.jsx
+++ src/Pages/CreateProject.jsx
@@ -5,7 +5,11 @@ const CreateProject = () => {
     <div className="font-lato">
       <div className="w-full shadow-[-5px_15px_50px_0px_rgba(5,78,18,0.05)] h-[32rem] bg-fuchsia-300 rounded-[30px] px-10">
         <p className="text-[#333] text-[40px] font-bold">Create new project</p>
+        <div className="bg-[url(./assets/CreateProjectImages/circle.svg)] h-[5rem] w-[4.3rem]  bg-no-repeat flex justify-center items-center text-xl">
+          1
+        </div>
       </div>
+
       <div className="w-full flex justify-center items-center  mt-10 mb-10"></div>
     </div>
   );
