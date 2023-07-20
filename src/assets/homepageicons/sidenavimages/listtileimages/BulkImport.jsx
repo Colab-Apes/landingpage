9a6a9393ac5022b8b2a867beb9b0bd0ab2d40457
himarnoel <olaimarnoel@gmail.com>
2023-07-20 12:04:54 +0100

    creating a bult import

---
+++ src/assets/homepageicons/sidenavimages/listtileimages/BulkImport.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import add from "./add.svg";
+import profile from "./profile.svg";
+import project from "./project.svg";
+import learning from "./learning.svg";
+import settings from "./settings.svg";
+import help from "./help.svg";
+import signout from "./signout.svg";
+
+const BulkImport = () => {
+  return <div>BulkImport</div>;
+};
+
+export default BulkImport;
