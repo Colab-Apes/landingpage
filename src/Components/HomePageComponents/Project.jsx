9f92f3c23501a1560a9616147b23070d55690865
himarnoel <olaimarnoel@gmail.com>
2023-07-21 10:01:17 +0100

    kep adjusting

--- src/Components/HomePageComponents/Project.jsx
+++ src/Components/HomePageComponents/Project.jsx
@@ -7,25 +7,25 @@ import hand from "../../assets/homepageicons/images/group-3.svg";
 import like from "../../assets/homepageicons/images/like.svg";
 const Project = ({ image, name }) => {
   return (
-    <div className=" w-full  font-lato flex flex-col  shadow-[3px_15px_50px_0px_rgba(5,78,18,0.10)] hover:shadow-sm cursor-pointer ease-out hover:duration-200 min-h-[34rem] sm:min-h-[35rem] md:min-h-[35rem] lg:min-h-[30rem] xl:min-h-[34rem] rounded-[30px] ">
+    <div className=" w-full  font-lato flex flex-col  shadow-[3px_15px_50px_0px_rgba(5,78,18,0.10)] hover:shadow-sm cursor-pointer ease-out hover:duration-200 min-h-[34rem] sm:min-h-[35rem] md:min-h-[35rem] lg:min-h-[30rem] xl:min-h-[35rem] rounded-[30px] pb-4">
       <img src={image} alt="" className="object-cover w-full " />
-      <div className="px-4 sm:px-4 lg:px-4 w-full text-lg lg:text-xs flex-1 rounded-b-[30px] flex flex-col justify-evenly">
+      <div className="px-4 sm:px-4 lg:px-4 w-full text-lg lg:text-xs flex-1 rounded-b-[30px] flex flex-col justify-evenly xl:px-6">
         <div className="flex flex-col text-base sm:text-2xl lg:text-base gap-y-3 sm:gap-y-6  lg:gap-y-3 text-[#333]">
           <div className="flex gap-x-3 text-xl sm:text-3xl  lg:text-base">
             <img
               src={bag}
               alt=""
-              className="object-contain sm:w-[1.9rem] lg:w-[1rem]"
+              className="object-contain sm:w-[1.9rem] lg:w-[1rem] xl:w-[1.5rem]"
             />
-            <p className="font-bold text-xl">{name}</p>
+            <p className="font-bold text-xl xl:text-2xl">{name}</p>
           </div>
           <div className="flex gap-x-3">
             <img
               src={group}
               alt=""
-              className="object-contain  sm:w-[1.9rem]  w-[1rem] lg:w-[1.2rem]"
+              className="object-contain  sm:w-[1.9rem]  w-[1rem] lg:w-[1.2rem] xl:w-[1.5rem]"
             />
-            <p className="text-[#999] font-semibold sm:text-base lg:text-base">
+            <p className="text-[#999] font-semibold sm:text-base lg:text-base xl:text-lg">
               Multiple owners
             </p>
           </div>
@@ -33,9 +33,9 @@ const Project = ({ image, name }) => {
             <img
               src={info}
               alt=""
-              className="object-contain  sm:w-[1.9rem]  w-[1rem] lg:w-[1rem]"
+              className="object-contain  sm:w-[1.9rem]  w-[1rem] lg:w-[1rem] xl:w-[1.5rem]"
             />
-            <p className="text-[#999999] sm:text-base lg:text-sm  font-semibold xl:text-lg   ">
+            <p className="text-[#999999] sm:text-base lg:text-sm  font-medium xl:text-[1rem] xl:leading-[1.5rem] ">
               Lorem ipsum dolor sit amet consectetur. Justo feugiat pharetra eu
               eu enim...
             </p>
@@ -50,17 +50,18 @@ const Project = ({ image, name }) => {
                 className="object-contain w-[1rem] sm:w-[1.9rem] lg:w-full"
               />
             </span>
-            <p>19+</p>
+            <p className="mr-6 font-bold text-sm">19+</p>
+            <div className="flex items-center gap-x-1">
+              <img
+                src={hand}
+                alt=""
+                className="object-contain  sm:w-[2.2rem] lg:w-full xl:w-[1.2rem]"
+              />
+              <p className="font-bold text-sm">5</p>
+            </div>
           </div>
-          <div className="flex items-center gap-x-1">
-            <img
-              src={hand}
-              alt=""
-              className="object-contain  sm:w-[2.2rem] lg:w-full"
-            />
-            <p>5</p>
-          </div>
-          <button className="h-[2rem] w-[8rem] sm:h-[2rem] sm:w-[8rem] lg:h-[2rem] lg:w-[6rem] border border-solid border-[#999] rounded-[15px] hover:text-white hover:bg-gradient-to-bl hover:from-[#054E12] hover:to-[#3B8004]">
+
+          <button className="h-[2rem] w-[8rem] sm:h-[2rem] sm:w-[8rem] lg:h-[2rem] lg:w-[6rem] xl:w-[8rem] xl:h-[2.5rem] border-[1px] border-solid border-[#999] rounded-[15px] hover:text-white hover:bg-gradient-to-bl hover:from-[#054E12] hover:to-[#3B8004] xl:text-base font-">
             Join project
           </button>
         </div>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -21,7 +21,7 @@ const HomePage = () => {
         </p>
       </p>
 
-      <div className="grid sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 mt-4 sm:mt-20 lg:mt-8 xl:mt-10 gap-y-10 sm:gap-y-20 md:gap-x-20 sm:gap-x-10 md:gap-y-32 lg:gap-y-10  lg:gap-x-4  xl:gap-x-6 xl:gap-y-10  mx-auto justify-center w-full ">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 mt-4 sm:mt-20 lg:mt-8 xl:mt-10 gap-y-10 sm:gap-y-20 md:gap-x-20 sm:gap-x-10 md:gap-y-32 lg:gap-y-10  lg:gap-x-4  xl:gap-x-4 xl:gap-y-10  mx-auto justify-center w-full ">
         <Project image={img1} name="Mobile game" />
         <Project image={img2} name="Product design" />
         <Project image={img3} name="Video game" />
@@ -36,7 +36,7 @@ const HomePage = () => {
         <Project image={img4} name="Data analysis" />
       </div>
       <div className="w-full flex justify-center items-center  mt-10 mb-10">
-        <button className="h-[2rem] mx-auto  w-[8rem] sm:h-[3rem] sm:w-[12rem] lg:h-[2.6rem] lg:w-[8rem] border border-solid border-[#999] text-[#999] rounded-[15px] ">
+        <button className="h-[2rem] mx-auto  w-[8rem] sm:h-[3rem] sm:w-[12rem] lg:h-[2.6rem] lg:w-[8rem] xl:h-[3rem] xl:w-[10rem] xl:text-xl border border-solid border-[#999] text-[#999] rounded-[15px] font-bold ">
           Load more...
         </button>
       </div>
